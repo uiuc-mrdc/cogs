6249d675146c90339f276c9d24a41a037f798000
Michael Gale <mgale456@gmail.com>
2019-11-08 23:53:04 -0600

    implimented a Websocket sending a basic message

--- django/composeexample/routing.py
+++ django/composeexample/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import team_management.routing
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            team_management.routing.websocket_urlpatterns
+        )
+    ),
 })
