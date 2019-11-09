25d3b8bce0dfb968f94d5fcd3e306850628bcd5a
Michael Gale <mgale456@gmail.com>
2019-11-09 11:11:07 -0600

    Attempted adding rows to the db through the websocket. Did not get it working, but close

--- django/team_management/admin.py
+++ django/team_management/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ScoringType, Team
+from .models import ScoringType, Team, LiveScore
 
 admin.site.register(ScoringType)
 admin.site.register(Team)
+admin.site.register(LiveScore)
--- django/team_management/consumers.py
+++ django/team_management/consumers.py
@@ -1,5 +1,7 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
+from django.utils import timezone
+from .models import LiveScore, ScoringType
 
 class GameConsumer(WebsocketConsumer):
     def connect(self):
@@ -10,8 +12,19 @@ class GameConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        action_id = text_data_json['action_id']
+        team_id = text_data_json['team_id']
+        print(text_data_json)
+        #self.send(text_data=json.dumps({
+        #    'message': message
+        #}))
+        print(ScoringType.objects.filter(id=2))
+        
+        #item = LiveScore(action_id="Brew Potion", time=timezone.now(), team_id=team_id)
+        #item.save()
+        
+        print(LiveScore.objects.all())
+        item2 = LiveScore.objects.get(id=1)
+        print(item2.action_id)
+        
+        #self.send(str(item.action_id))
