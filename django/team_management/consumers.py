13370ae3a0e9e67f9dc82509b5a98353c7e3fabd
Michael Gale <mgale456@gmail.com>
2019-11-09 17:35:30 -0600

    Standard buttons insert a row in the scoring database without an error
    Breaks if no team is selected

--- django/team_management/consumers.py
+++ django/team_management/consumers.py
@@ -1,7 +1,7 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
 from django.utils import timezone
-from .models import LiveScore, ScoringType
+from .models import LiveScore, ScoringType, Team
 
 class GameConsumer(WebsocketConsumer):
     def connect(self):
@@ -12,19 +12,15 @@ class GameConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        action_id = text_data_json['action_id']
-        team_id = text_data_json['team_id']
+        action_id = ScoringType.objects.get(pk=text_data_json['action_id'])
+        team_id = Team.objects.get(pk=text_data_json['team_id'])
         print(text_data_json)
         #self.send(text_data=json.dumps({
         #    'message': message
         #}))
-        print(ScoringType.objects.filter(id=2))
-        
-        #item = LiveScore(action_id="Brew Potion", time=timezone.now(), team_id=team_id)
-        #item.save()
+        item = LiveScore(action_id=action_id, time=timezone.now(), team_id=team_id)
+        item.save()
         
         print(LiveScore.objects.all())
-        item2 = LiveScore.objects.get(id=1)
-        print(item2.action_id)
         
-        #self.send(str(item.action_id))
+        self.send('success!')
