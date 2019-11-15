0adbc35118ca6981dd73ba3e229114dbad1cec77
Michael Gale <mgale456@gmail.com>
2019-11-15 17:40:38 -0600

    separated handling of counters and standard buttons on the webpage and server side
    Also rearanged for a bit of better readability, I think

--- django/team_management/consumers.py
+++ django/team_management/consumers.py
@@ -12,32 +12,35 @@ class GameConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        func = self.switcher.get(text_data_json['type']) 
+        func = self.switcher.get(text_data_json['type']) #switcher is at the bottom
         return func(self, text_data_json) #These two lines implement a sort of switch statement based on the type to differentiate between adding/deleting/etc.
         
-    def addLiveScore(self, json_data):
+    def standardLiveScore(self, json_data):
         scoring_type = ScoringType.objects.get(pk=json_data['scoringType_id'])
         team = Team.objects.get(pk=json_data['team_id'])
-        print(json_data)
-        #self.send(text_data=json.dumps({
-        #    'message': message
-        #}))
-        item = LiveScore(scoring_type=scoring_type, time=timezone.now(), team=team)
-        item.save()
+        live_score = LiveScore(scoring_type=scoring_type, time=timezone.now(), team=team)
+        live_score.save()
         
         self.send(text_data=json.dumps({
             'team_name':team.team_name,
-            'liveScore_id':item.id,
+            'liveScore_id':live_score.id,
             'scoringType_name':scoring_type.name,
             'scoringType_id':scoring_type.id,
-            'time':str(item.time),
+            'time':str(live_score.time),
         }))
     
-    def deleteLiveScore(self, json_data):
-        print(json_data)
-        LiveScore.objects.get(pk=json_data['scoringType_id']).delete() #Passes LiveScore.id as ScoringType.id to save data space
+    def counterLiveScore(self, json_data):
+        scoring_type = ScoringType.objects.get(pk=json_data['scoringType_id'])
+        team = Team.objects.get(pk=json_data['team_id'])
+        live_score = LiveScore(scoring_type=scoring_type, time=timezone.now(), team=team)
+        live_score.save()
+        #note there is no response, since it doesn't need a delete button
     
-    switcher = {
+    def deleteLiveScore(self, json_data):
+        LiveScore.objects.get(pk=json_data['scoringType_id']).delete() #Passes LiveScore.id as ScoringType.id to save data space
+        
+    switcher = { #must be defined after the functions
         'delete':deleteLiveScore,
-        'addLiveScore':addLiveScore,
+        'standardLiveScore':standardLiveScore,
+        'counterLiveScore':counterLiveScore,
     }
