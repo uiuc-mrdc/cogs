3250e008399dd1700a70c0166ec47781b75411d9
Michael Gale <mgale456@gmail.com>
2019-11-27 17:44:26 -0600

    Added function to the GameParticipant Model class to count actions in the database for a specific team
    still needs to calculate score, but counts are there in a weird format ish

--- django/team_management/models.py
+++ django/team_management/models.py
@@ -36,13 +36,39 @@ class GameParticipant(models.Model):
     score = models.IntegerField(default=0)
     def __str__(self):
         return ", ".join(["Game " + str(self.game.id), self.team.team_name])
+    def score(self):
+        counts_list = []
+        for scoring_type in ScoringType.objects.all():
+            counts_list.append(self.counts(scoring_type))
+        return 0
+        #return some function of all the counts
+    def counts(self, scoring_type):
+        base = Action.objects.filter(
+                deleted = False
+            ).filter(
+                team = self.team
+            ).filter(
+                scoring_type = scoring_type
+            )
+        counts_multipliers = []
+        print(base.values('multiplier').distinct())
+        for mult in base.values('multiplier').distinct():
+            mult_table = base.filter(multiplier = mult['multiplier'])
+            up = mult_table.filter(
+                    upDown = True
+                ).count()
+            down = mult_table.filter( #note only counter style inputs have any "down" values
+                    upDown = False
+                ).count()
+            counts_multipliers.append((up-down, mult['multiplier']))
+        return counts_multipliers
     
 class Action(models.Model):
     scoring_type = models.ForeignKey(ScoringType, on_delete=models.DO_NOTHING)
     time = models.DateTimeField()
     team = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
     game = models.ForeignKey(Game, on_delete=models.DO_NOTHING)
-    multiplier = models.DecimalField(max_digits=2, decimal_places=1, default=1)
+    multiplier = models.FloatField(default=1)
     upDown = models.BooleanField(default=1)
     deleted = models.BooleanField(default=False)
     def __str__(self):
