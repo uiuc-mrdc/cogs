2e4ed4864797af02e2ae7be23dccb7e8399db7b1
Logan Pulley <logan@pulley.host>
2020-02-01 14:08:29 -0600

    Django3 upgrade (#14)
    * Updated requirements for Django 3.0
    
    * Converted to MySQL/MariaDB
    
    * Fixed Docker named volume container directory for MariaDB
    
    * Fixed models.py errors caused by Blog being abstract
    
    * implimented django-dbconn-retry
    
    does not fix startup issue, but may be good anyways
    
    * Revert "implimented django-dbconn-retry"
    
    This reverts commit 11b934b7cfec6fbc81ad2e265ae529ff8d55d40e.
    
    Reverting commit, since the add on did not work as we hoped. You can go to the commit and try it if you wish.
    
    * Uses 'MRDC' user for the db now. No longer creates 'root' twice in the docker-compose file. Also removed MYSQL_ROOT_HOST, since django seems to work without it
    
    * Lowercased db username
    
    * Updated documentation with database initialization
    
    Co-authored-by: mgale456 <55673155+mgale456@users.noreply.github.com>

--- django/team_management/models.py
+++ django/team_management/models.py
@@ -1,17 +1,10 @@
-from djongo import models
+from django.db import models
 
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField(blank=True)
     author = models.CharField(max_length=50)
 
-    class Meta:
-        abstract = True
-
 class Entry(models.Model):
-    blog = models.EmbeddedModelField(
-        model_container=Blog,
-    )
-
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     headline = models.CharField(max_length=255)
-    objects = models.DjongoManager()
