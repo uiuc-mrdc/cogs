561f27784e9f2f6a5d0bd4bbb1d85809e72ad85a
Logan Pulley <logan@pulley.host>
2020-02-01 13:16:02 -0600

    Lowercased db username

--- django/composeexample/settings.py
+++ django/composeexample/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'MRDC',
+        'USER': 'mrdc',
         'PASSWORD': 'apricots',
         'HOST': 'db',
         'PORT': 3306
