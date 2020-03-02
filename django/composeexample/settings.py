f4f870499721eecaeba8af60d93cd1cc250a0463
Michael Gale <mgale456@gmail.com>
2020-03-02 15:39:10 -0600

    added basic auth pages from a tutorial. Need to add in features still to the other pages.

--- django/composeexample/settings.py
+++ django/composeexample/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'composeexample.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'composeexample/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
