4490ae8d90781619e49add4bfa27265ee64f0dea
Michael Gale <mgale456@gmail.com>
2020-03-05 01:31:33 -0600

    emails work now through gmail, but you must enable less secure apps

--- django/composeexample/settings.py
+++ django/composeexample/settings.py
@@ -146,4 +146,9 @@ os.path.join(BASE_DIR, "static"),
 
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER= 'mgale456@gmail.com'
+EMAIL_HOST_PASSWORD = ####use your own account #For Gmail, you must enable "Allow less secure apps"
+EMAIL_USE_TLS = True
