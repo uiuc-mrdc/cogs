117c14b2d422fe8b2617b8896623692165de23b5
Michael Gale <mgale456@gmail.com>
2020-10-04 19:27:05 -0500

    added game-participant relation to the Game api fields

--- django/rest_app/serializers.py
+++ django/rest_app/serializers.py
@@ -4,9 +4,10 @@ from team_management.models import Game, Team, ScoringType
 
 #Rename to Match
 class GameSerializer(serializers.HyperlinkedModelSerializer):
+    gameparticipant_set = serializers.PrimaryKeyRelatedField(many=True, read_only=True) #we can redefine the name of this in models.py with 'related_name'
     class Meta:
         model = Game
-        fields = ['url', 'id', 'start_time', 'end_time', 'finished', 'pause_time']
+        fields = ['url', 'id', 'start_time', 'end_time', 'finished', 'pause_time', 'gameparticipant_set']
 		
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
