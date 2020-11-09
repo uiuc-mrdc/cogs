20a984b2f3e02384198d46905f42f8ec248c78a2
Michael Gale <mgale456@gmail.com>
2020-11-08 20:34:10 -0600

    minor updates to models.py making fields optional, adding help text, and adding Regex validation for phone numbers

--- django/team_management/models.py
+++ django/team_management/models.py
@@ -3,6 +3,7 @@ from django.db.models import Sum
 from django.utils import timezone
 from datetime import timedelta
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 class School(models.Model):
     name = models.CharField(max_length=70)
@@ -15,7 +16,7 @@ class Team(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=70)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    abbreviation = models.CharField(max_length=8, default="")
+    abbreviation = models.CharField(max_length=8, default="", blank=True)
     weigh_in = models.BooleanField(default=False)
     safety_check = models.BooleanField(default=False)
     #logo?
@@ -25,12 +26,12 @@ class Team(models.Model):
 class TeamContact(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     email = models.EmailField(blank=True)
-    phone_number = models.CharField(max_length=15)
-
-
+	#regex source: Brooke's comment on https://stackoverflow.com/questions/123559/how-to-validate-phone-numbers-using-regex
+	#We may want to update this to match whatever texting service we use, or to be more strict
+    phone_number = models.CharField(max_length=15, blank=True, validators=[RegexValidator(regex='(?:(?:(\s*\(?([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\)?\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})')])
 
 class Match(models.Model):
-    special_name = models.CharField(max_length=50, default="") #stores data for special games. ie. 1 means it is a semifinal #not used yet
+    special_name = models.CharField(max_length=50, default="", blank=True, help_text="i.e. Semifinal. Currently unused")
     scheduled_start = models.DateTimeField(default = timezone.now() + timedelta(minutes=10)) #remove default value once we have a way to set this better
     class Meta:
         verbose_name_plural = "Matches"
@@ -144,17 +145,17 @@ class ScoringTypeGroup(models.Model):
     
 class ScoringType(models.Model): #Table for every way to score. #Autogenerates buttons on the judging page
     name = models.CharField(max_length=40)
-    limit = models.IntegerField(default=0)
+    limit = models.IntegerField(default=0, help_text="Maximum number of times this can be scored. 0 for no limit.")
     score_change = models.IntegerField()
-    extra_data = models.CharField(max_length=10) #used to set colors on the judging page for counters
-    scoring_type_group = models.ForeignKey(ScoringTypeGroup, on_delete=models.CASCADE) #specifies how the judging screen should display it
+    extra_data = models.CharField(max_length=10, blank=True, help_text="Used to display extra things, such as color on the judging page") #used to set colors on the judging page for counters
+    scoring_type_group = models.ForeignKey(ScoringTypeGroup, on_delete=models.CASCADE, help_text="Used to group ScoringTypes into display groups for the judging page") #specifies how the judging screen should display it
     def __str__(self):
         return self.name
     
 class ScoringEvent(models.Model): #Table for every scoring event
     scoring_type = models.ForeignKey(ScoringType, on_delete=models.CASCADE)
-    Contender = models.ForeignKey(Contender, on_delete=models.CASCADE)
+    contender = models.ForeignKey(Contender, on_delete=models.CASCADE)
     match_timestamp = models.DurationField(default=timedelta(minutes=0))
     count = models.IntegerField(default=1)
     def __str__(self):
-        return ", ".join(["Game " + str(self.game_participant.game_id), str(self.scoring_type), str(self.game_participant.team)])
+        return " | ".join(["Match " + str(self.contender.match_id), str(self.scoring_type), str(self.contender.team)])
