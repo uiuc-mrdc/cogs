a873b89be0c72bc59c2a0e1ac04882185634599a
Michael Gale <mgale456@gmail.com>
2020-11-10 18:25:31 -0600

    updated contender to HyperlinkedModelSerializer to match all other serializers. Added nested link to ContenderPosition, so we can know the color in a single query. Updated a comment

--- django/rest_app/serializers.py
+++ django/rest_app/serializers.py
@@ -18,15 +18,21 @@ class SchoolSerializer(serializers.HyperlinkedModelSerializer):
         model = School
         fields = '__all__'
 
-class ContenderSerializer(serializers.ModelSerializer):
+class ContenderPositionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ContenderPosition
+        fields = '__all__'
+
+class ContenderSerializer(serializers.HyperlinkedModelSerializer):
     team = TeamSerializer(read_only=True)
     score = serializers.IntegerField(max_value=None, min_value=None, source='calculate_score', read_only=True)
+    contender_position = ContenderPositionSerializer(read_only=True)
     class Meta:
         model = Contender
         fields = '__all__'
 
 class MatchSerializer(serializers.HyperlinkedModelSerializer):
-    #gameparticipant_set = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='gameparticipant-detail') #we can redefine the name of this in models.py with 'related_name'
+    #contenders = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='contender-detail') #This is the same as below in end result
     contenders = ContenderSerializer(many=True, read_only=True) #we can rename this in models.py with related_name
     class Meta:
         model = Match
@@ -37,10 +43,7 @@ class MatchStateChangeEventSerializer(serializers.HyperlinkedModelSerializer):
         model = MatchStateChangeEvent
         fields = '__all__'
         
-class ContenderPositionSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ContenderPosition
-        fields = '__all__'
+
 
 class ContenderContextChangeEventSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
